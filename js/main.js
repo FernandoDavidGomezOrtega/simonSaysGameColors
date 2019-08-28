8868e60ecac00f1e62a18430046a2cd90f566dd9
Fernando David Gómez Ortega <ferdgomez@hotmail.es>
2019-08-28 15:48:36 +0200

    theems added and level counter

--- js/main.js
+++ js/main.js
@@ -1,14 +1,43 @@
-swal('Bienvenido a Simón dice', 'Intenta seguir la secuencia. \nEl juego consta de 10 niveles. \nSuerte!', 'info')
-
-const celeste = document.getElementById('celeste')
-const violeta = document.getElementById('violeta')
-const naranja = document.getElementById('naranja')
-const verde = document.getElementById('verde')
-const btnEmpezar = document.getElementById('btnEmpezar')
-const ULTIMO_NIVEL = 10
-
-class Juego {
-  constructor() {
+$(document).ready(function () {
+  swal('Bienvenido a Simón dice', 'Intenta seguir la secuencia. \nEl juego consta de 10 niveles. \nSuerte!', 'info')
+  
+  // Selector de tema
+  var theme = $('#theme')
+  
+  $('#to-gray').click(function () {
+    theme.attr('href', 'css/gray_theme.css')
+  })
+  
+  $('#to-green').click(function () {
+    theme.attr('href', 'css/green_theme.css')
+  })
+  
+  $('#to-red').click(function () {
+    theme.attr('href', 'css/red_theme.css')
+  })
+  
+  $('#to-blue').click(function () {
+    theme.attr('href', 'css/blue_theme.css')
+  })
+  
+  $('#to-black').click(function () {
+    theme.attr('href', 'css/black_theme.css')
+  })
+  
+  ////////////////////////////////////
+  
+})
+  
+  
+  const celeste = document.getElementById('celeste')
+  const violeta = document.getElementById('violeta')
+  const naranja = document.getElementById('naranja')
+  const verde = document.getElementById('verde')
+  const btnEmpezar = document.getElementById('btnEmpezar')
+  const ULTIMO_NIVEL = 10
+  
+  class Juego {
+    constructor() {
     this.inicializar = this.inicializar.bind(this)
     this.inicializar()
     this.generarSecuencia()
@@ -36,7 +65,7 @@ class Juego {
       btnEmpezar.classList.add('hide')
     }
   }
-
+  
   generarSecuencia() {
     this.secuencia = new Array(ULTIMO_NIVEL).fill(0).map(n => Math.floor(Math.random() * 4))
   }
@@ -52,28 +81,28 @@ class Juego {
     switch (numero) {
       case 0:
         return 'celeste'
-      case 1:
+        case 1:
         return 'violeta'
-      case 2:
-        return 'naranja'
+        case 2:
+          return 'naranja'
       case 3:
         return 'verde'
     }
   }
-
+  
   transformarColorANumero(color) {
     switch (color) {
       case 'celeste':
         return 0
-      case 'violeta':
-        return 1
-      case 'naranja':
-        return 2
-      case 'verde':
-        return 3
-    }
-  }
-
+        case 'violeta':
+          return 1
+          case 'naranja':
+            return 2
+            case 'verde':
+              return 3
+            }
+          }
+          
   iluminarSecuencia() {
     for (let i = 0; i < this.nivel; i++) {
       const color = this.transformarNumeroAColor(this.secuencia[i])
@@ -85,7 +114,7 @@ class Juego {
     this.colores[color].classList.add('light')
     setTimeout(() => this.apagarColor(color), 350);
   }
-
+  
   apagarColor(color) {
     this.colores[color].classList.remove('light')
   }
@@ -116,32 +145,40 @@ class Juego {
         if (this.nivel === (ULTIMO_NIVEL + 1)) {
           this.ganoElJuego()
         } else {
-          swal('Muy bien!', 'Pasas al siguiente nivel', 'success')
+          swal('Muy bien!', 'Pasas al nivel ' + this.nivel, 'success')
           .then(() => {
             setTimeout(this.siguienteNivel, 1500)
           })
-
+          
         }
       }
     } else {
       this.perdioElJuego()
     }
   }
-
+  
   ganoElJuego() {
-    swal('Simón dice:', 'Felicitaciones, ganaste el juego!', 'success')
-      .then(this.inicializar)
+    swal('Simón dice:', 'FELICITACIONES, ganaste el juego!', 'success')
+    .then(this.inicializar)
   }
-
+  
   perdioElJuego() {
     swal('Simón dice:', 'Lo lamentamos, perdiste :(', 'error')
-      .then(() => {
-        this.eliminarEventosClick()
-        this.inicializar()
-      })
+    .then(() => {
+      this.eliminarEventosClick()
+      this.inicializar()
+    })
   }
+
+
+  
 }
 
+
+
 function empezarJuego() {
-  window.juego = new Juego()
-}
+    window.juego = new Juego()
+  }
+
+
+  
